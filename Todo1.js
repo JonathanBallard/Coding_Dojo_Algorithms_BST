26561ad8b4f2900e6bda7e0cc9e4ed9feae3752d
Jonathan Ballard <asgardschosen@gmail.com>
2020-03-27 16:18:09 -0700

    initial commit

---
+++ Todo1.js
@@ -0,0 +1,29 @@
+
+
+function BTNode(value) {
+    this.val = value;
+    this.left = null;
+    this.right = null;
+}
+function BST() {
+    this.root = null;
+    // add methods here...
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
