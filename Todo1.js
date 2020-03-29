6274c98f92ad42ad84a42ca9db1f3c04b5d4e141
Jonathan Ballard <asgardschosen@gmail.com>
2020-03-28 20:57:52 -0700

    add and contains

--- Todo1.js
+++ Todo1.js
@@ -1,14 +1,100 @@
 
 
-function BTNode(value) {
-    this.val = value;
-    this.left = null;
-    this.right = null;
+class BTNode {
+    constructor(value){
+        this.val = value;
+        this.left = null;
+        this.right = null;
+    }
 }
-function BST() {
-    this.root = null;
+class BST{
+    constructor(){
+        this.root = null;
+    }
     // add methods here...
+
+
+
+    add(val){
+        if(this.root != null){
+            var runner = this.root;
+            while(runner){
+                if(val > runner.val){
+                    if(runner.right != null){
+                        runner = runner.right;
+                    }
+                    else {
+                        runner.right = new BTNode(val);
+                        return this;
+                    }
+                }
+                else if(val >= runner.val){
+                    if(runner.left != null){
+                        runner = runner.left;
+                    }
+                    else {
+                        runner.left = new BTNode(val);
+                        return this;
+                    }
+                }
+
+            }
+        }
+        else {
+            this.root = new BTNode(val)
+            return this;
+        }
+    }
+
+
+    contains(val){
+        var runner = this.root;
+        if(runner){
+            if(runner.val == val){
+                return "found";
+            }
+            else if(val > runner.val){
+                if(runner.right != null){
+                    runner = runner.right;
+                }
+                else{
+                    return "not found"
+                }
+            }
+            else if(val < runner.val){
+                if(runner.left != null){
+                    runner = runner.left;
+                }
+                else{
+                    return "not found"
+                }
+            }
+            
+        }
+        else {
+            return "not found";
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+var myBST = new BST();
+myBST.add(10).add(50);
+console.log(myBST);
+
+
 
 
 
